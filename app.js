9d0bfe2f8081a2b6b235835511c8ffeb1d6b2ccc
James Ned Bobo <harleyzone1@gmail.com>
2015-11-15 19:15:02 -0800

    Nov 15 2015 7:14PM

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var data;
 //search by song title
-var baseUrl = 'https://api.spotify.com/v1/search?type=track&query='
-//search by artist name
-
-//search by album name
-
+var baseUrl = 'https://api.spotify.com/v1/search?type=track&limit=25&query='
+var artistUrl = 'artist%3A*';
+var albumUrl = 'album%3A*';
+var genreUrl = 'genre%3A*';
+var url2 = '*&type=track';
+var searchBy;
 var myApp = angular.module('myApp', [])
 var albumlg = '/img/placeholder.png';
 var albummd = '/img/placeholder.png';
@@ -14,12 +15,63 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http, $window) {
     $scope.width = 0;
     $scope.albumArt = albumlg;
 
+    $scope.sortType = 'name';
+    $scope.sortReverse = '';
+
+    $scope.searchBy = [
+    {type:'Song Title'},
+    {type:'Artist Name'},
+    {type:'Album Title'},
+    {type:'Genre'}
+    //{type:'Release Year'}
+    //track.album.release_date doesn't seem to be working on spotify yet. it just returns as empty for every item. I can do a successful query when I search for a certain release date too, but is just returns empty...
+  ];
+  $scope.search = $scope.searchBy[0]; // Default search song titles
+
+
   $scope.audioObject = {}
   $scope.getSongs = function() {
-    $http.get(baseUrl + $scope.track).success(function(response){
-      data = $scope.tracks = response.tracks.items
+    if($scope.track != undefined){
+            if ($scope.search == $scope.searchBy[0]){
+            $http.get(baseUrl + $scope.track).success(function(response){
+                data = $scope.tracks = response.tracks.items
+            })
+        }else if ($scope.search == $scope.searchBy[1]){
+            var search = (artistUrl + $scope.track + url2);
+            $http.get(baseUrl + search).success(function(response){
+                data = $scope.tracks = response.tracks.items
+            })
+        } else if ($scope.search == $scope.searchBy[2]){
+            var search = (albumUrl + $scope.track + url2);
+            $http.get(baseUrl + search).success(function(response){
+                data = $scope.tracks = response.tracks.items
+            })
+        } else if ($scope.search == $scope.searchBy[3]){
+            var search = (genreUrl + $scope.track + url2);
+            $http.get(baseUrl + search).success(function(response){
+                data = $scope.tracks = response.tracks.items
+            })
+        }
+        $scope.move();
+    }
+    //$(window).scrollTop($('#table').offset().top);
+  }
 
-    })
+  $scope.move = function(){
+      $('html,body').animate({
+       scrollTop: $("#searchForm").offset().top},
+       'slow');
+  }
+
+  $scope.artist = function(artist){
+      var artist = artist;
+      $scope.search = $scope.searchBy[1];
+      $scope.track = artist;
+  }
+  $scope.album = function(album){
+      var album = album;
+      $scope.search = $scope.searchBy[2];
+      $scope.track = album;
   }
   $scope.play = function(song) {
     if($scope.currentSong == song) {
@@ -32,6 +84,9 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http, $window) {
       $scope.audioObject = new Audio(song);
       $scope.audioObject.play()
       $scope.currentSong = song
+      $('html,body').animate({
+       scrollTop: $("#albumArt").offset().top},
+       'slow');
     }
   }
   $scope.art = function(lg, md, sm) {
